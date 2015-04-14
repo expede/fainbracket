84122698ce2f8a8cb2688979a5cc1bd29b8de985
Brooklyn Zelenka <be.zelenka@gmail.com>
2015-04-13 23:11:40 -0700

    Char literal notation, remove dead code

--- lexer/lexer.rkt
+++ lexer/lexer.rkt
@@ -2,29 +2,19 @@
 
 (provide lexer)
 
-(define valid-inputs
-  (set "<"
-       ">"
-       "."
-       ","
-       "+"
-       "-"
-       "["
-       "]"))
-
 (define (char->token character)
   (case character
-    ["<"  'shift-left]
-    [">"  'shift-right]
-    ["."  'print-cell]
-    [","  'overwrite-cell]
-    ["+"  'increment-cell]
-    ["-"  'decrement-cell]
-    ["["  'loop-start]
-    ["]"  'loop-end]
+    [#\<  'shift-left]
+    [#\>  'shift-right]
+    [#\.  'print-cell]
+    [#\,  'overwrite-cell]
+    [#\+  'increment-cell]
+    [#\-  'decrement-cell]
+    [#\[  'loop-start]
+    [#\]  'loop-end]
     [else 'ignore]))
 
-(define (lexer string)
+(define (lexer raw-string)
   (filter-not
     (lambda [x] (= x 'ignore))
-    (map char->token string)))
+    (map char->token (string->list raw-string))))
