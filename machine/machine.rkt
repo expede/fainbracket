cd97e7e013a34ab743f46ffb0fcb66afedf2922b
Brooklyn Zelenka <be.zelenka@gmail.com>
2015-04-14 00:00:45 -0700

    More placeholders

--- machine/machine.rkt
+++ machine/machine.rkt
@@ -2,5 +2,15 @@
 
 (provide run-machine)
 
+
 (define (run-machine machine tape)
-  machine tape)
+  (let ([cell (first tape)])
+    (case (first machine)
+      ['print-cell (write cell)])))
+
+(define (step machine tape))
+(define (left machine))
+(define (right machine))
+(define (increment cell))
+(define (decrement cell))
+(define (overwrite cell))
